f54911a32d92a2d0929cda44f88251855c42100d
Ash <ashe.null@protonmail.ch>
2020-10-05 16:16:46 -0500

    oops i forgot to add basically anything

---
+++ src/H3MP.Server/Program.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Threading;
+using LiteNetLib;
+using LiteNetLib.Utils;
+
+namespace H3MP.Server
+{
+	public static class Program
+	{
+		public static void Main()
+		{
+			var listener = new EventBasedNetListener();
+			var server = new NetManager(listener);
+
+			Console.Write("key: ");
+			var key = Console.ReadLine();
+
+			listener.ConnectionRequestEvent += request => 
+			{
+				request.AcceptIfKey(key);
+			};
+
+			listener.PeerConnectedEvent += peer => 
+			{
+				Console.WriteLine($"Peer connected: {peer.EndPoint}");
+
+				Console.Write("Type a response: ");
+				var response = Console.ReadLine();
+
+				var writer = new NetDataWriter();
+				writer.Put(response);
+				peer.Send(writer, DeliveryMethod.ReliableSequenced);
+			};
+
+			server.Start(9099);
+
+			while (Console.ReadKey().Key != ConsoleKey.Q) 
+			{
+				server.PollEvents();
+				Thread.Sleep(10);
+			}
+		}
+	}
+}
