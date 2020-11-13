89a6688c44579d8cccc8cde039495d5efd6688d6
M'aiq <m.janik1031@gmail.com>
2020-11-13 15:47:40 -0600

    refactor & make this not so much trash

--- src/H3MP/Utils/WristMenuButtons.cs
+++ src/H3MP/Utils/WristMenuButtons.cs
@@ -1,8 +1,10 @@
 using BepInEx.Logging;
 using FistVR;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
@@ -13,7 +15,7 @@ namespace H3MP.Utils
 		private readonly ManualLogSource _log;
 
 		private const float BUTTON_SIZE = 31f;
-		private const float BUTTON_PLACEMENT_DIF = 0.01565f;
+		private const float BUTTON_PLACEMENT_DIFF = 0.01565f;
 
 		public WristMenuButtons(ManualLogSource log)
 		{
@@ -38,43 +40,74 @@ namespace H3MP.Utils
 			// Get the canvas and sets
 			var canvasTF = wristMenu.transform.Find("MenuGo/Canvas");
 			var canvasRT = canvasTF.GetComponent<RectTransform>();
-			canvasRT.sizeDelta = new Vector2(canvasRT.sizeDelta.x, canvasRT.sizeDelta.y + BUTTON_SIZE);
+			canvasRT.sizeDelta = new Vector2(canvasRT.sizeDelta.x, canvasRT.sizeDelta.y + (BUTTON_SIZE*2));
+			var canvasBS = canvasTF.GetComponent<OptionsPanel_ButtonSet>();
+			var originalButtonsLength = canvasBS.ButtonImagesInSet.Length;
 
-			// Get necessary base buttons
-			var reloadScene = canvasTF.Find("Button_3_ReloadScene").gameObject;
-			var quitToDesktop = canvasTF.Find("Button_10_QuitToDesktop").gameObject;
+			// Create butons, resize array, and fill in our new buttons to the array
+			var buttons = CreateButtons(canvasTF).ToList();
+			Array.Resize(ref canvasBS.ButtonImagesInSet, originalButtonsLength + buttons.Count);
+			for (var i = 0; i < buttons.Count; ++i)
+			{
+				var button = buttons[i];
+				wristMenu.Buttons.Add(button);
+				
+				var image = button.GetComponent<Image>();
+				var pointable = button.GetComponent<FVRWristMenuPointableButton>();
+				var index = originalButtonsLength+i;
+				
+				pointable.ButtonIndex = index;
+				canvasBS.ButtonImagesInSet[index] = image;
+			}
+		}
 
-			// Create our button
-			// TODO: create button from scratch instead?
-			var disconnect = GameObject.Instantiate(reloadScene, reloadScene.transform.parent);
-			disconnect.transform.position = new Vector2(0, quitToDesktop.transform.position.y);
-			disconnect.name = "H3MP_Disconnect";
-			disconnect.GetComponentInChildren<Text>().text = "Leave Lobby Session";
+		private IEnumerable<Button> CreateButtons(Transform canvas)
+		{
+			var source = canvas.Find("Button_3_ReloadScene").gameObject;
+			Button CreateButton(string posName, string name, string text, bool delete, UnityAction callback)
+			{
+				var posTransform = canvas.Find(posName);
+				posTransform.position += BUTTON_PLACEMENT_DIFF * 2 * Vector3.down;
 
-			// Add the actual button component to ours
-			var disconnectBtn = disconnect.GetComponent<Button>();
-			disconnectBtn.onClick.AddListener(LeaveLobby);
+				var dest = GameObject.Instantiate(source, posTransform.position, posTransform.rotation, posTransform.parent);
+				dest.transform.position += BUTTON_PLACEMENT_DIFF * 2 * Vector3.up;
+				dest.name = name;
+				dest.GetComponentInChildren<Text>().text = text;
 
-			// Move base quit button down
-			quitToDesktop.transform.position = new Vector2(0, quitToDesktop.transform.position.y - BUTTON_PLACEMENT_DIF);
+				var button = dest.GetComponent<Button>();
 
-			// Add our button to the list & set the index
-			wristMenu.Buttons.Add(disconnectBtn);
+				if (delete) {
+					Component.DestroyImmediate(button);
+					button = dest.AddComponent<Button>();
+				}
 
-			// Fix up the button lists
-			// TODO: this still looks like trash?
-			var buttonSet = canvasTF.GetComponent<OptionsPanel_ButtonSet>();
-			var wristMenuPointableButton = disconnect.GetComponent<FVRWristMenuPointableButton>();
-			wristMenuPointableButton.ButtonIndex = buttonSet.ButtonImagesInSet.Length;
-			Array.Resize(ref buttonSet.ButtonImagesInSet, buttonSet.ButtonImagesInSet.Length + 1);
-			buttonSet.ButtonImagesInSet[wristMenuPointableButton.ButtonIndex] = disconnect.GetComponent<Image>();
+				button.onClick.AddListener(callback);
+
+				return button;
+			}
+
+			void MoveButton(string posName)
+			{
+				var posTransform = canvas.Find(posName);
+				posTransform.position += BUTTON_PLACEMENT_DIFF * 2 * Vector3.down;
+			}
+
+			yield return CreateButton("Button_3_ReloadScene", "H3MP_Privacy", "Open Party", true, PrivacySelector);
+			yield return CreateButton("Button_9_BackToMainMenu", "H3MP_Disconnect", "Leave Party", false, LeaveLobby);
+			MoveButton("Button_10_QuitToDesktop");
 		}
 
 		private void LeaveLobby()
 		{
 			//TODO: make this actually leave session after merge with feature/netcode-refactor
 			//TODO: maybe make this take two clicks like reloadscene/quit buttons?
-			_log.LogDebug("Left H3MP session");
+			_log.LogDebug("Left H3MP party");
+		}
+
+		private void PrivacySelector()
+		{
+			//TODO: maybe make this cycle lobby privacy & set button text to reflect that
+			_log.LogDebug("Changed party privacy");
 		}
 
 		public void Dispose()
